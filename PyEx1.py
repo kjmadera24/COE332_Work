7f6d3233304c1e66a1edb7f7318bf27246273640
Kamilla Madera <kjmadera@utexas.edu>
2023-01-24 01:20:34 -0600

    Delete changes

--- PyEx1.py
+++
@@ -1,7 +0,0 @@
-long5 = []
-with open('/home/kjm24/words', 'r') as f:
-    long5 = f.read().splitlines()
-
-long5.sort(key=len, reverse = True)
-for x in range(5):
-    print(long5[x])
--- PyEx3.py
+++
@@ -1,9 +0,0 @@
-import names
-
-def namelen(value):
-    return(len(value) - 1)
-
-for i in range(5):
-    name = names.get_full_name()
-    length = namelen(name)
-    print(name + " - " + str(length))
