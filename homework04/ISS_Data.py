7c3943107225475d4aa8516f0a4a47eacd1a5b0d
Ubuntu <ubuntu@kjm24-vm.js2local>
2023-02-21 16:34:31 +0000

    cleaning the main py

--- homework04/ISS_Data.py
+++ homework04/ISS_Data.py
@@ -9,12 +9,5 @@ def main():
     ISSData = xmltodict.parse(APIReq.text)
     ISSD = ISSData['ndm']['oem']['body']['segment']['data']['stateVector'])  #Just a small shortcut
 
-    Tavg =  CurrentTurb(TD, 'calibration_constant', 'detector_current')
-
-    if Tavg <= 1:
-        print("Average turbidity based on the most recent five measurements = " + str(Tavg) + " NTU \nInfo: Turbidity is below threshold for safe use \nNo time required to reach threshold")
-    else:
-        print("Average turbidity based on the most recent five measurements = " + str(Tavg) + " NTU \nWarning: Turbidity is above threshold for safe use \nThe minimum time required to return below a safe threshold = " + str(MinTime(Tavg)) + " hours")
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
